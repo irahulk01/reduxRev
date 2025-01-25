de1d7643445f29ee612f053b1edcac478a2af551
Rahul Kumar <irahulkv@outlook.com>
2025-01-25 19:05:16 +0530

    updated local files

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 let state = {
-    count :0
+    count: 0
+};
+
+let prevState =  state;
+
+function increment(){
+state.count = state.count + 1
 }
-console.log(state.state = 1)
+increment()
+console.log(state)
+increment()
+console.log(state)
+increment()
+console.log(state)
+increment()
+console.log(state)
